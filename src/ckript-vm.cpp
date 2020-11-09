487e6f1d78bbbaa26f3d13a1e1449a52c170591a
Roller23 <roller47@wp.pl>
2020-11-09 17:32:34 +0100

    added native functions

--- src/ckript-vm.cpp
+++ src/ckript-vm.cpp
@@ -4,6 +4,8 @@
 
 #include <cassert>
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 bool Value::is_lvalue() {
   return reference_name.size() != 0;
@@ -55,6 +57,60 @@ void Heap::free(Variable *var) {
 
 // stdlib
 
+static std::string stringify(Value &val) {
+  if (val.type == Utils::VarType::STR) {
+    return val.string_value;
+  }
+  if (val.type == Utils::VarType::INT) {
+    return std::to_string(val.number_value);
+  }
+  if (val.type == Utils::VarType::FLOAT) {
+    return std::to_string(val.float_value);
+  }
+  if (val.type == Utils::VarType::FUNC) {
+    return "function";
+  }
+  if (val.type == Utils::VarType::BOOL) {
+    return val.boolean_value ? "true" : "false";
+  }
+  if (val.type == Utils::VarType::CLASS) {
+    return "class " + val.class_name;
+  }
+  if (val.type == Utils::VarType::VOID) {
+    return "void";
+  }
+  if (val.type == Utils::VarType::UNKNOWN) {
+    return "null";
+  }
+  if (val.type == Utils::VarType::ARR) {
+    std::string str = "array(";
+    int i = 0;
+    for (auto &el : val.array_values) {
+      str += stringify(el);
+      if (i != val.array_values.size() - 1) {
+        str += ", ";
+      }
+      i++;
+    }
+    str += ")";
+    return str;
+  }
+  if (val.type == Utils::VarType::OBJ) {
+    std::string str = "object(";
+    int i = 0;
+    for (auto &member : val.member_values) {
+      str += member.first + ": " + stringify(member.second);
+      if (i != val.member_values.size() - 1) {
+        str += ", ";
+      }
+      i++;
+    }
+    str += ")";
+    return str;
+  }
+  return "";
+}
+
 class NativeInput : public NativeFunction {
   public:
     Value execute(std::vector<Value> &args, std::int64_t line) {
@@ -77,7 +133,26 @@ class NativePrint : public NativeFunction {
       }
       std::cout << args.at(0).string_value;
       Value val(Utils::VarType::VOID);
-      return {val};
+      return val;
+    }
+};
+
+class NativeSize : public NativeFunction {
+  public:
+    Value execute(std::vector<Value> &args, std::int64_t line) {
+      if (args.size() != 1) {
+        ErrorHandler::throw_runtime_error("size() expects one argument", line);
+      }
+      Value &arg = args.at(0);
+      Value val(Utils::VarType::INT);
+      if (arg.type == Utils::ARR) {
+        val.number_value = arg.array_values.size();
+      } else if (arg.type == Utils::STR) {
+        val.number_value = arg.string_value.size();
+      } else {
+        ErrorHandler::throw_runtime_error("Cannot get the size of " + stringify(arg), line);
+      }
+      return val;
     }
 };
 
@@ -87,10 +162,88 @@ class NativeTostr : public NativeFunction {
       if (args.size() != 1) {
         ErrorHandler::throw_runtime_error("to_str() expects one argument", line);
       }
-      
       Value val(Utils::VarType::STR);
-      val.string_value = "";
-      return {val};
+      val.string_value = stringify(args.at(0));
+      return val;
+    }
+};
+
+class NativeToint : public NativeFunction {
+  public:
+    Value execute(std::vector<Value> &args, std::int64_t line) {
+      if (args.size() != 1) {
+        ErrorHandler::throw_runtime_error("to_int() expects one argument", line);
+      }
+      Value &arg = args.at(0);
+      Value val(Utils::INT);
+      if (arg.type == Utils::INT) {
+        val.number_value = arg.number_value;
+      } else if (arg.type == Utils::FLOAT) {
+        val.number_value = (std::int64_t)arg.float_value;
+      } else if (arg.type == Utils::STR) {
+        char *endptr;
+        val.number_value = std::strtoll(arg.string_value.c_str(), &endptr, 0);
+        if (*endptr != 0) {
+          ErrorHandler::throw_runtime_error(arg.string_value + " cannot be converted to int", line);
+        }
+      } else if (arg.type == Utils::BOOL) {
+        val.number_value = (std::int64_t)arg.boolean_value;
+      } else {
+        ErrorHandler::throw_runtime_error(stringify(arg) + " cannot be converted to int", line);
+      }
+      return val;
+    }
+};
+
+class NativeTodouble : public NativeFunction {
+  public:
+    Value execute(std::vector<Value> &args, std::int64_t line) {
+      if (args.size() != 1) {
+        ErrorHandler::throw_runtime_error("to_double() expects one argument", line);
+      }
+      Value &arg = args.at(0);
+      Value val(Utils::FLOAT);
+      if (arg.type == Utils::INT) {
+        val.float_value = (double)arg.number_value;
+      } else if (arg.type == Utils::FLOAT) {
+        val.float_value = arg.float_value;
+      } else if (arg.type == Utils::STR) {
+        char *endptr;
+        val.float_value = std::strtod(arg.string_value.c_str(), &endptr);
+        if (*endptr != 0) {
+          ErrorHandler::throw_runtime_error(arg.string_value + " cannot be converted to double", line);
+        }
+      } else if (arg.type == Utils::BOOL) {
+        val.float_value = (double)arg.boolean_value;
+      } else {
+        ErrorHandler::throw_runtime_error(stringify(arg) + " cannot be converted to double", line);
+      }
+      return val;
+    }
+};
+
+class NativeExit : public NativeFunction {
+  public:
+    Value execute(std::vector<Value> &args, std::int64_t line) {
+      if (args.size() != 1 || args.at(0).type != Utils::VarType::INT) {
+        ErrorHandler::throw_runtime_error("exit() expects one argument (int)", line);
+      }
+      std::exit(args.at(0).number_value);
+      return {};
+    }
+};
+
+class NativeTimestamp : public NativeFunction {
+  public:
+    Value execute(std::vector<Value> &args, std::int64_t line) {
+      if (args.size() != 0) {
+        ErrorHandler::throw_runtime_error("timestamp() expects no arguments", line);
+      }
+      Value val(Utils::INT);
+      val.number_value = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+      ).count();
+      return val;
     }
 };
 
@@ -101,4 +254,19 @@ void CkriptVM::load_stdlib(void) {
   NativePrint *print = new NativePrint;
   NativeFunction *print_ptr = print;
   globals.insert(std::make_pair("print", print_ptr));
+  NativeTostr *to_str = new NativeTostr;
+  NativeFunction *to_str_ptr = to_str;
+  globals.insert(std::make_pair("to_str", to_str_ptr));
+  NativeExit *exit = new NativeExit;
+  NativeFunction *exit_ptr = exit;
+  globals.insert(std::make_pair("exit", exit_ptr));
+  NativeToint *to_int = new NativeToint;
+  NativeFunction *to_int_ptr = to_int;
+  globals.insert(std::make_pair("to_int", to_int_ptr));
+  NativeTodouble *to_double = new NativeTodouble;
+  NativeFunction *to_double_ptr = to_double;
+  globals.insert(std::make_pair("to_double", to_double_ptr));
+  NativeSize *size = new NativeSize;
+  NativeFunction *size_ptr = size;
+  globals.insert(std::make_pair("size", size_ptr));
 }
