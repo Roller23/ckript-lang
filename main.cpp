6c0cf494b3d873b9a7f5932e8c33cbdeca4bb614
Wiktor Kania <roller47@wp.pl>
2021-01-21 19:47:38 +0100

    changed main() structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include "src/interpreter.hpp"
 
 int main(int argc, char *argv[]) {
-  Interpreter interpreter;
   if (argc == 1) {
-    interpreter.process_stream();
-    return 0;
+    Interpreter().process_stream();
+  } else {
+    Interpreter().process_file(argv[1], argc - 1, argv + 1);
   }
-  interpreter.process_file(argv[1], argc - 1, argv + 1);
   return 0;
 }
